78f37431f1b47e53f1d1cfc88d19df3b09fddf76
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-12-02 05:42:56 -0300

    Initial commit

---
+++ src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
@@ -0,0 +1,13 @@
+package me.dio.academia.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/academia/digital/AcademiaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.academia.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
