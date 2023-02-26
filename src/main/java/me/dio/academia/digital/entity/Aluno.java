8cc4987a4ab5af5069d7d21f630e9f5a671f9363
Edielson Assis <105529988+edielson-assis@users.noreply.github.com>
2023-02-26 17:16:19 -0300

    Modificado GenerationType para IDENTITY

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class Aluno {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String nome;
